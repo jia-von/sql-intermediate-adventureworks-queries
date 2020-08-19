ce82574b90dbf0608a934fdb102b7f436a0b7679
Jia <jiavon@ualberta.ca>
2020-08-19 16:12:25 -0600

    the total employee that first name starts with S is 1281

--- AssignmentSQL.sql
+++ AssignmentSQL.sql
@@ -8,8 +8,16 @@ SELECT MAX(VacationHours) as MaxVacation FROM employee;
 
 --Join two tables together with ContactID using unique table/alias
 SELECT c.FirstName, c.LastName, c.ContactID FROM contact c 
-INNER JOIN employee e ON e.ContactID = c.ContactID WHERE c.ContactID = '1209' 
+INNER JOIN employee e ON e.ContactID = c.ContactID WHERE c.ContactID = '1209' --Output is Guy Gilbert. 
 
---Name is Guy Gilbert. 
+--Query: Determine how many employees there are whose names start with the letter S.
+
+--According to the DataDictionary, employee table has ContactID foreign key. I filtered all the names with ContactID IS NOT NULL in contact table, therefore all employee should have ContactID in contact table. 
+
+--According to the table there is a total of 1281, however I will use the COUNT function to compute. 
+--The answer matches
+
+SELECT COUNT(FirstName), ContactID FROM contact WHERE ContactID IS NOT NULL AND FirstName LIKE 'S%'; -- Output COUNT(FirstName) = 1281
+
 
 
